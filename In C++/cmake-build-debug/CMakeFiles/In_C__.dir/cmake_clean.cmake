e9cae9413d6a0e5d6a99ee3fe7645364f3f8c2a9
Karthik Kolathumani <karthik_kr@hotmail.com>
2021-12-24 11:53:31 -0800

    C++ Setup Files Wtf

---
+++ In C++/cmake-build-debug/CMakeFiles/In_C__.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/In_C__.dir/main.cpp.o"
+  "In_C__"
+  "In_C__.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/In_C__.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
